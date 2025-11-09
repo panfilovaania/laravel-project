d3731b4d48a12fe1e00f90368f9883f6f89e7451
panfilovaania <panfilovaania@mail.ru>
2025-11-09 22:52:45 +0300

    Add blade views

---
+++ resources/views/pages/sign-in.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+
+@section('title', 'Вход')
+
+@section('content')
+    @include('components.sign-in-form')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/', 'pages.main');
+Route::view('/sign-in', 'pages.sign-in');
+Route::view('/sign-up', 'pages.sign-up');
+Route::view('/profile', 'pages.profile');
