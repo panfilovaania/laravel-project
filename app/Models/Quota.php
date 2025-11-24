5c4f125a30935355415200439032434fb5da60c2
panfilovaania <52575636+panfilovaania@users.noreply.github.com>
2025-11-24 20:42:45 +0300

    Add models, factories and seeders (#3)

---
+++ app/Models/Quota.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Quota extends Model
+{
+    use HasFactory;
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'phone' => $this->faker->numerify('+79#########'),
+            'birthday' => $this->faker->date()
         ];
     }
 
@@ -38,7 +38,6 @@ class UserFactory extends Factory
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
         ]);
     }
 }
